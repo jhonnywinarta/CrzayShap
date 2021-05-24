eca5dc5abcaf3a6ddd62c1b8dcad9f37b32d92e0
jhonnywinarta <jhonnywinarta@gmail.com>
2021-05-24 23:17:40 +0800

    2021上機考v2_(黃裕洳)

--- app/src/main/java/tw/edu/pu/o1083023/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o1083023/crazyshape/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -13,6 +14,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(this, "Author:黃裕洳", Toast.LENGTH_LONG).show()
+
+        Glide.with(this)
+            .load(R.drawable.cover)
+            .override(800, 600)
+            .into(imgTitle)
 
         imgNext.setOnLongClickListener(object: View.OnLongClickListener{
             override fun onLongClick(v: View?): Boolean {
